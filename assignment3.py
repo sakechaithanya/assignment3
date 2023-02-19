ac1e5927f2a68c8ca396a982edb26f1cc2f52b57
SAKE CHAITANYA <61873017+sakechaithanya@users.noreply.github.com>
2023-02-19 21:33:13 +0530

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,5 +1,10 @@
+#1q
 total= 0
 list=[8,2,3,0,7]
 for i in range(0,len(list)):
   total=total+list[i]
 print("sum of all elements in given list 8+2+3+0+7:",total)
+#2q
+txt="1234abcd"[::-1]
+print(txt)
+#3q
